5ad626d1d536d0213064eaec4f1941bf0a40090b
Emil Militzer <emil.militzer@nordakademie.de>
2017-09-18 21:00:10 +0200

    Umstellung der Einleseverfahren auf eine Utillity Klasse

--- Software Engineering/src/de/nordakademie/se/Studentenverwaltung.java
+++ Software Engineering/src/de/nordakademie/se/Studentenverwaltung.java
@@ -3,10 +3,10 @@ package de.nordakademie.se;
 import de.nordakademie.se.studenten.Student;
 import de.nordakademie.se.studenten.konkretestudenten.AinfStudent;
 import de.nordakademie.se.studenten.konkretestudenten.WingStudent;
+import de.nordakademie.se.util.Konsole;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * @author Emil Militzer
@@ -27,9 +27,7 @@ public class Studentenverwaltung {
     /**
      * Erzeugung eines neuen Studenten.
      */
-    private void registriereNeuenStudent() {
-        Scanner scanner = new Scanner(System.in);
-
+    private void registriereNeuenStudent()  {
         int type = -1;
         //So lange keine vorhandene Studentenart ausgewählt wird, erfolgt die Abfrage
         while (type < 0 || type >= 2) {
@@ -38,44 +36,45 @@ public class Studentenverwaltung {
             System.out.println("0) angewandte Informatik");
             System.out.println("1) Wirtschaftsingenieurwesen");
 
-            type = scanner.nextInt();
+            type = Konsole.readInt();
             System.out.println();
         }
+
+        //Abfrage der nötigen allgemeinen Daten
+        System.out.println("Bitte Name eingeben: ");
+        String name = Konsole.readString();
+        System.out.println("Bitte Jahrgang eingeben: ");
+        int jahrgang = Konsole.readInt();
+        Student student = null;
+
+        //Unterscheidung der Studentenart
         if (type == 0) {
-            //Abfrage der nötigen Daten
-            System.out.println("Bitte Name eingeben: ");
-            String name = scanner.next();
-            System.out.println("Bitte Jahrgang eingeben: ");
-            int jahrgang = scanner.nextInt();
+
+            //Abfrage der speziellen Eigenschaft eines Angewandten Informatikers
             System.out.println("Bitte Lieblingsprogrammiersprache eingeben");
-            String programmiersprache = scanner.next();
+            String programmiersprache = Konsole.readString();
 
             //Erstellung des Studenten
-            AinfStudent ainfStudent = new AinfStudent(programmiersprache);
-            ainfStudent.setName(name);
-            ainfStudent.setJahrgang(jahrgang);
+            student = new AinfStudent(programmiersprache);
 
-            //Hinzufügen des Studenten in die Liste
-            studenten.add(ainfStudent);
         } else if (type == 1) {
-            //Abfrage der nötigen Daten
-            System.out.println("Bitte Name eingeben: ");
-            String name = scanner.next();
-            System.out.println("Bitte Jahrgang eingeben");
-            int jahrgang = scanner.nextInt();
+
+            //Abfrage der speziellen Eigenschaft eines Wirtschaftsingenieurs
             System.out.println("Lieblingswerkzeug eingeben:");
-            String lieblingswerkzeug = scanner.next();
+            String lieblingswerkzeug = Konsole.readString();
             System.out.println("Praktikumsstunden eingeben:");
-            int praktikumsstunden = scanner.nextInt();
+            int praktikumsstunden = Konsole.readInt();
 
             //Erstellung des Studenten
-            WingStudent wingStudent = new WingStudent(lieblingswerkzeug, praktikumsstunden);
-            wingStudent.setLieblingswerkzeug(lieblingswerkzeug);
-            wingStudent.setPraktikumsstunden(praktikumsstunden);
-
-            //Hinzufügen des Studenten in die Liste
-            studenten.add(wingStudent);
+            student = new WingStudent(lieblingswerkzeug, praktikumsstunden);
         }
+
+        //Setzen der allgemeinen Felder
+        student.setName(name);
+        student.setJahrgang(jahrgang);
+
+        //Hinzufügen des Studenten in die Liste
+        studenten.add(student);
     }
 
     /**
@@ -101,10 +100,9 @@ public class Studentenverwaltung {
             System.out.println("0) Verlassen");
             System.out.println("1) neuen Student registrieren");
             System.out.println("2) alle Studenten anzeigen");
-            Scanner scanner = new Scanner(System.in);
 
             //Einlesen der gewählten Aktion
-            auswahl = scanner.nextInt();
+            auswahl = Konsole.readInt();
             System.out.println();
 
             //Auswahl zwischen den Menüpunkten
---
+++ Software Engineering/src/de/nordakademie/se/util/Konsole.java
@@ -0,0 +1,51 @@
+package de.nordakademie.se.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Emil Militzer
+ *
+ *
+ * Hilfsklasse für das Einlesen von Strings und Zahlen aus der Konsole
+ *
+ * Irrelevant für das Pattern.
+ */
+public class Konsole {
+
+    private static BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * Liest einen String aus der Konsole ein.
+     *
+     * @return eingelesenen String
+     */
+    public static String readString(){
+        try {
+            return bufferReader.readLine();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Liest einen int aus der Konsole ein.
+     *
+     * @return eingelesene int
+     */
+    public static int readInt(){
+        int val = 0;
+        boolean erfolgreich = false;
+        while(!erfolgreich) {
+            try {
+                val =  Integer.valueOf(bufferReader.readLine().trim());
+                erfolgreich = true;
+            } catch (IOException | NumberFormatException e) {
+                erfolgreich = false;
+                System.out.println("Die Eingabe war keine Zahl, bitte erneut versuchen.");
+            }
+        }
+        return val;
+    }
+}
